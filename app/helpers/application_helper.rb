d093d76424489a445c8611628219acea81224934
Ram Iyer <ramky.iyer@yahoo.com>
2014-11-14 11:35:50 -0800

    using green flash success message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def alert_class_for(flash_type)
     {
-      :success => 'alert-success',
       :error => 'alert-danger',
       :alert => 'alert-warning',
-      :notice => 'alert-info'
+      :notice => 'alert-success'
+      #:whatever => 'alert-info'
 
     }[flash_type.to_sym] || flash_type.to_s
   end
