7e0a340194eae8cd8af059a9c2cab112856366fa
Ram Iyer <ramky.iyer@yahoo.com>
2014-11-13 16:18:55 -0800

    adding friendly time

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,15 @@
 class Task < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
   belongs_to :user
 
   validates_presence_of :title, :description, :user
+
+  def my_deadline
+    deadline = time_ago_in_words(self.deadline)
+    if self.deadline < Time.now
+      "#{deadline} ago"
+    else
+      "#{deadline} from now"
+    end
+  end
 end
