8de4c78442cbfb5108774c279824fc94f1f9659a
Ram Iyer <ramky.iyer@yahoo.com>
2014-12-18 11:15:47 -0600

    fixing test - removing username and adding email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe User, type: :model do
   it { should have_many(:tasks) }
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
-  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
 end
